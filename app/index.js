c90fc5ea9abdb5f65a6921fc9cf52c6c1f2497a3
Taco <taco-tues@tuta.io>
2020-01-21 11:57:56 -0700

    Build index.js

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+import ReactDom from "react-dom";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>name-dot-com</h1>
+    </React.Fragment>
+  );
+}
+
+// render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+ReactDom.render(<App />, rootElement);
