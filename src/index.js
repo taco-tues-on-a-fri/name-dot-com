be791e00f2085e90bd6f37fb824ff0bbf8ccbe47
Mike Jadick <mike@jadick.dev>
2020-05-29 01:03:19 -0600

    Adjust margin top

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./index.css";
 import ReactDom from "react-dom";
-// import SnowFlakes from "./components/Snow";
-// import Mountains from "./components/Mountains";
 
 function App() {
   return (
@@ -21,11 +19,11 @@ function App() {
           </li>
         </ul>
       </nav>
-      <div>
-        <img className="waves" src="./src/static/cerulean-wave.svg" />
-      </div>
-      <div>
+      <img className="waves" src="./src/static/cerulean-wave.svg" />
       <h1 className="greeting">Hi, I'm Michael Jadick</h1>
+      <div className="card">
+        <h2 className="card-title">Full Stack Developer</h2>
+        <p>I specialize in taco related code.</p>
       </div>
     </React.Fragment>
   );
