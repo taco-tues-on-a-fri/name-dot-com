20983428f532eeaa60a1a1004f08f0985de20da1
Mike Jadick <mike@jadick.dev>
2020-06-06 12:03:00 -0600

    Adjust regex for babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.js$/, use: 'babel-loader' },
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]},
       { 
         test: /\.(png|svg|jpg|gif)$/,
